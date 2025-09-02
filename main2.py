ca7903887c9154882e4f9da5619c15eec1005345
a.dolgov <a.dolgov@zebrains.team>
2025-09-02 15:00:57 +0000

    feature/foreign-recognition

--- main2.py
+++ main2.py
@@ -4,11 +4,30 @@ import numpy as np
 
 if __name__ == "__main__":
     passport_recognitor = PassportRecognitor()
+
+    paths = [
+        "data/dataset/6.jpg",
+        "data/images/sasha_2.jpg",
+        "data/images/danil.jpg",
+        "data/dataset/24.jpg",
+        "data/images/2.jpg"
+        ]
+    
+    for path in paths:
+        print("-" * 5 + path + "-" * 5)
+        image = np.array(Image.open(path))
+        passport_data = passport_recognitor.extract_passport_data(image)
+        print(passport_data)
+
     # image = np.array(Image.open("data/2312660261.jpg"))
-    # image = np.array(Image.open("data/sasha_2.jpg"))
-    image = np.array(Image.open("data/dataset/24.jpg"))
+    # image = np.array(Image.open("data/dataset/6.jpg"))
+    # image = np.array(Image.open("data/images/sasha.jpg"))
+    # image = np.array(Image.open("data/images/sasha_2.jpg"))
+    # image = np.array(Image.open("data/images/danil.jpg"))
+    # image = np.array(Image.open("data/dataset/24.jpg"))
+    # image = np.array(Image.open("data/images/2.jpg"))
     
 
-    passport_data = passport_recognitor.extract_passport_data(image)
+    # passport_data = passport_recognitor.extract_passport_data(image)
 
-    print(passport_data)
+    # print(passport_data)
