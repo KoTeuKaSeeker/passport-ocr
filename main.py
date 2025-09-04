dd8b94377e7654bb35247a5e92f46f36dc025ec6
d.dolgov <d.dolgov@zebrains.team>
2025-09-04 11:34:33 +0000

    kisdhf

--- main.py
+++ main.py
@@ -28,47 +28,16 @@ async def recognize_passport(file: UploadFile = File(...)):
     image_pil = Image.open(io.BytesIO(contents)).convert("RGB")
     image = np.array(image_pil)
 
-    passport_fields = recognitor.extract_passport_data(image)
-
-    # passport_fields = {
-    #     'passport_surname': 'ДОЛГОВ', 
-    #     'passport_name': 'АЛЕКСАНДР', 
-    #     'passport_patronymic': 'ПЕТРОВИЧ', 
-    #     'passport_sex': 'M', 
-    #     'passport_birth_date': '10.05.03', 
-    #     'passport_birth_place': '1ox', 
-    #     'passport_series': '7323', 
-    #     'passport_number': '542628', 
-    #     'passport_issue_date': '03.07.23', 
-    #     'passport_department_code': '730-001', 
-    #     'passport_issued_by': "GET THIS INFORMATION FROM 'depratment_code'",\
-    #     "passport_citizenship": "Российсая Федерация ТЕСТ",
-    #     "passport_mrz": "P<RUSDOLOV<<ALEKSANDR<PETROVICH<<<<<<<<<<<<<<<<<<\n7323542628RUS0305103M2307036<<<<<<<<<<<<<<04"
-    # }
-
-    # foreign_fields = {
-    #     "foreign_doc_type": "P",
-    #     "foreign_country_code": "RUS",
-    #     "foreign_citizenship": "RUS",
-    #     "foreign_latin_name": "IVANOV IVAN",
-    #     "foreign_birth_date": "10.05.2003",
-    #     "foreign_sex": "M",
-    #     "foreign_birth_place": "г. Ульяновск",
-    #     "foreign_number": "XXXXXXX",
-    #     "foreign_issue_date": "21.02.2023",
-    #     "foreign_expiry_date": "21.02.2027",
-    #     "foreign_issued_by": "УМВД и ещё чего-то там...",
-    #     "foreign_mrz": "<<><><>><kek<><><>lol>>><>><>><><><><<>"
-    # }
-
-    # passport_type = 'passport' if random.random() > 0.5 else 'foreign'
-
-    # пока вернём "заглушку"
+    try:
+        passport_fields = recognitor.extract_passport_data(image)
+    except Exception as e:
+        passport_fields = {"error": str(e)}
+        
     result = {
         "filename": file.filename,
         "content_type": file.content_type,
         "size": len(contents),
-        "passport_type": passport_fields["passport_type"],
+        "passport_type": passport_fields.get("passport_type", "passport"),
         "fields": passport_fields
     }
 
